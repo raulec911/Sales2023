865c1ca57ec46b0ed7d34a78dafd914c186bc9d7
Raul Sanchez <raulec911@hotmail.com>
2023-03-26 18:04:50 -0500

    Begin Countries Index

--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -1,11 +1,16 @@
+global using Sales.Shared.Entities;
+global using Sales.WEB.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.WEB;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7150/") });
+
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
